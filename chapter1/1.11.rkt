2a437696adf8684dfbedf9b5565ecc62109998a6
Semyon Pupkov <mail@semyonpupkov.com>
2019-01-21 14:03:22 +0500

    Add iter

--- chapter1/1.11.rkt
+++ chapter1/1.11.rkt
@@ -1,11 +1,24 @@
 #lang racket/base
 
-; three recurcive
+;recurcive
 (define (f n)
   (cond ((< n 3) n)
         (else (+ (f (- n 1))
                  (f (- n 2))
                  (f (- n 3))))))
 
-(f 1)
-(f 10)
+;(f 1)
+;(f 10)
+
+;iter
+
+(define (f2 n)
+  (define (f-iter a b c count)
+    (if (= count 0)
+        c
+        (f-iter (+ a b c) a b (- count 1))
+        ))
+  (f-iter 2 1 0 n))
+
+(f2 1)
+(f2 10)
